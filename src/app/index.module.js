2a3c7e7c8fc6f724745054ce53da499fdce17a5a
veena.n.v@gmail.om <aitraaz>
2017-04-21 14:48:45 -0700

    changes according to the new Angula

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('myfrontend', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('myfrontend')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController($scope,$log) {
+    var vm = this;
+
+
+    vm.creationDate = 1492798393079;
+
+
+$scope.postMessage=function(){
+ $log.log("test message1");
+
+}
+
+
+
+
+  }
+})();
