03d91b569b49e8be4aa89a6b69fd6a66308a29d1
veena.n.v@gmail.om <aitraaz>
2017-04-24 23:06:36 -0700

    server side changes

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -12,16 +12,14 @@
     this.creationDate = 1492798393079;
 
     $scope.getMessage=function(){
-      alert("hello");
+
       $http.get('http://localhost:5000/api/message').then(function(result) {
         console.log("get request successfully");
         console.log(result);
-      }, function(data){
-        console.error("error in get");
       })
     }
 
-  $scope.postMessage=function(){
+    $scope.postMessage = function () {
 
    $http.post('http://localhost:5000/api/message',{'msg' :this.message}).success(function(data) {
       console.log("posted successfully");
